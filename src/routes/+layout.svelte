3bcf9e4f4cdedbccd1f98331fb98be50314aac69
Jaroslav Hort <hortjaroslav0@gmail.com>
2024-04-04 19:28:11 +0200

    feat: style background and header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,14 @@
 <script>
   import { page } from '$app/stores';
   import "../app.css";
+  import Header from '../lib/components/header.svelte'
 </script>
 
 <svelte:head>
 	<title>Portfolio</title>
 </svelte:head>
 
-<slot />
+<div class="h-screen w-screen bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] from-purple-900 via-gray-900 to-black text-white font-medium">
+  <Header/>
+  <slot />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,3 @@
-<script lang="ts">
-	import 'iconify-icon';
-</script>
-
-<div
-	class="w-screen h-screen flex justify-center items-center bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] from-purple-900 via-gray-900 to-black text-slate-100 text-opacity-700"
->
-	<div class="flex flex-col gap-3 items-center">
-		<h1 class="font-mono text-3xl">Jaroslav Hort _</h1>
-		<h2 class="font-mono text-lg">Portfolio under construction</h2>
-		<div class="flex flex-row gap-4 text-3xl justify-center items-center mt-1">
-			<a href="https://github.com/hortjar">
-				<iconify-icon icon="mdi:github"></iconify-icon>
-			</a>
-			<a href="https://www.linkedin.com/in/jaroslav-hort-4a9b01206/">
-				<iconify-icon icon="mdi:linkedin" class="text-4xl"></iconify-icon>
-			</a>
-		</div>
-	</div>
+<div class="grid">
+  This is the page
 </div>
