d2761f700fb2856a9ca79a6efceb7e71b97b1baa
Jaroslav Hort <hortjaroslav0@gmail.com>
2024-04-05 19:02:33 +0200

    feat: add initial button styling

---
+++ src/lib/components/project-link.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import type { projects } from '$lib/db/schema';
+  import type { InferSelectModel } from 'drizzle-orm';
+  import ProjectButton from './project-button.svelte';
+
+  export let project: InferSelectModel<typeof projects>;
+</script>
+
+{#if project.public}
+  <a href={project.url}>
+    <ProjectButton {project}></ProjectButton>
+  </a>
+{:else}
+  <div>
+    <ProjectButton {project}></ProjectButton>
+  </div>
+{/if}
