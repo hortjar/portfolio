54752f399656ff65e727e7d24f921b3623482c3f
Jaroslav Hort <hortjaroslav0@gmail.com>
2024-04-07 18:50:53 +0200

    feat: add new css media breakpoint to improve width

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
   export let data: PageData;
 </script>
 
-<div class="flex flex-col gap-3 w-full lg:w-1/2 xl:w-1/3 md:px-2">
+<div class="flex flex-col gap-3 w-full lg:w-2/3 xl:w-1/2 2xl:w-1/2 3xl:w-1/3 md:px-2">
   <InfoCard></InfoCard>
   <div class="flex flex-col xl:flex-row gap-3 flex-wrap">
     {#each data.projects as project}
